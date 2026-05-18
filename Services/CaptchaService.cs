2dbb2ab73608d1ea41f50b78d12fa6f0e53908b6
Denys <dankosickach@gmail.com>
2026-05-18 18:55:39 +0300

    7 - CaptchaService remarks

--- Services/CaptchaService.cs
+++ Services/CaptchaService.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
+using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
-using SixLabors.Fonts;
 
 namespace CommentSystem.Api.Services
 {
     public class CaptchaService : ICaptchaService
     {
         private readonly IMemoryCache _cache;
-        private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private const string Chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
 
         public CaptchaService(IMemoryCache cache) => _cache = cache;
 
@@ -22,14 +23,35 @@ namespace CommentSystem.Api.Services
             _cache.Set(captchaId, code, TimeSpan.FromMinutes(5));
 
             using var image = new Image<Rgba32>(150, 50);
+
+            Font font;
+            try
+            {
+                font = SystemFonts.CreateFont("Arial", 25, FontStyle.Bold);
+            }
+            catch
+            {
+                font = SystemFonts.Collection.Families.First().CreateFont(25, FontStyle.Bold);
+            }
+
             image.Mutate(ctx =>
             {
                 ctx.Fill(Color.White);
-                for (int i = 0; i < 10; i++)
+
+                var random = new Random();
+                for (int i = 0; i < 6; i++)
                 {
-                    ctx.DrawLines(Color.Silver, 1, new PointF(new Random().Next(150), new Random().Next(50)), new PointF(new Random().Next(150), new Random().Next(50)));
+                    var p1 = new PointF(random.Next(150), random.Next(50));
+                    var p2 = new PointF(random.Next(150), random.Next(50));
+                    ctx.DrawLine(Color.Silver, 1.5f, p1, p2);
                 }
-                ctx.DrawText(code, SystemFonts.CreateFont("Arial", 25), Color.Black, new PointF(20, 10));
+
+                ctx.DrawText(code, font, Color.Black, new PointF(20, 10));
+
+                for (int i = 0; i < 100; i++)
+                {
+                    ctx.Fill(Color.Gray, new EllipsePolygon(random.Next(150), random.Next(50), 1));
+                }
             });
 
             using var ms = new MemoryStream();
